c9efb591393c2b71ecb381ec53b7e7b12a167e90
Alex Parvanov <aparvanov@abv.bg>
2017-12-29 17:53:50 +0200

    Adding Viacheslav methods

--- AutoService/AutoService.Core/Engine/Engine.cs
+++ AutoService/AutoService.Core/Engine/Engine.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
+using System.Text;
 using AutoService.Core.Factory;
 using AutoService.Models.Assets;
 using AutoService.Models.Assets.Contracts;
 using AutoService.Models.BusinessProcess.Contracts;
 using AutoService.Models.BusinessProcess.Enums;
-using AutoService.Models.Common.Models;
 using AutoService.Models.Contracts;
-using AutoService.Models.Vehicles.Contracts;
 
 namespace AutoService.Core
 {
     public sealed class Engine : IEngine
     {
+        //TODO CHECK WHOLE DOCUMENT FOR REPETITIVE CODE
+
         private readonly IList<IEmployee> employees;
         private readonly ICollection<IAsset> bankAccounts;
         private readonly ICollection<ICounterparty> clients;
@@ -90,6 +89,12 @@ namespace AutoService.Core
         private void ExecuteSingleCommand(string[] commandParameters)
         {
             var commandType = commandParameters[0];
+            int employeeId;
+            decimal ratePerMinute;
+            IEmployee employee = null;
+            string position;
+            DepartmentType department;
+
             switch (commandType)
             {
                 case "showEmployees":
@@ -108,15 +113,15 @@ namespace AutoService.Core
                     }
                     var firstName = commandParameters[1];
                     var lastName = commandParameters[2];
-                    var position = commandParameters[3];
+                    position = commandParameters[3];
                     decimal salary = decimal.TryParse(commandParameters[4], out salary)
                         ? salary
                         : throw new ArgumentException("Please provide a valid decimal value for salary!");
-                    decimal ratePerMinute = decimal.TryParse(commandParameters[5], out ratePerMinute)
+                    ratePerMinute = decimal.TryParse(commandParameters[5], out ratePerMinute)
                         ? ratePerMinute
                         : throw new ArgumentException("Please provide a valid decimal value for rate per minute!");
                     ;
-                    DepartmentType department;
+
                     if (!Enum.TryParse(commandParameters[6], out department))
                     {
                         throw new ArgumentException("Department not valid!");
@@ -136,7 +141,7 @@ namespace AutoService.Core
                         throw new InvalidOperationException("No employees currently in the service!");
                     }
 
-                    int employeeId = int.TryParse(commandParameters[1], out employeeId)
+                    employeeId = int.TryParse(commandParameters[1], out employeeId)
                         ? employeeId
                         : throw new ArgumentException("Please provide a valid integer value!");
 
@@ -145,34 +150,223 @@ namespace AutoService.Core
                         throw new ArgumentException($"Please provide a valid employee value, i.e. between 1 and {this.employees.Count}!");
                     }
                     
-                    IEmployee employee = this.employees.Count >= employeeId
+                 employee = this.employees.Count >= employeeId
                         ? this.employees[employeeId - 1]
                         : throw new ArgumentException("This employee does not exist!");
                     this.FireEmployee(employee);
                     break;
-                case "changeEmployeesRate":
-                    firstName = commandParameters[1];
-                    lastName = commandParameters[2];
-                    var rate = decimal.Parse(commandParameters[3]);
-                    if (employees.Any(x => x.FirstName != firstName && x.LastName != lastName))
+                case "changeEmployeeRate":
+                    if (commandParameters.Length != 3)
                     {
-                        throw new ArgumentException("The are no employee with this name!");
+                        throw new NotSupportedException(
+                            "Change employer rate command must be with only 3 parameters!");
                     }
-                    foreach (var employe in employees)
+
+                    if (this.employees.Count == 0)
                     {
-                        if (employee.FirstName == firstName && employee.LastName == lastName)
-                        {
-                            employee.ChangeRate(rate);
-                            Console.WriteLine($"Employee {employee.FirstName.ToString()}{employee.LastName.ToString()} rate were changed to {rate}!");
-                        }
+                        throw new InvalidOperationException("No employees currently in the service!");
                     }
+
+                    employeeId = int.TryParse(commandParameters[1], out employeeId)
+                        ? employeeId
+                        : throw new ArgumentException("Please provide a valid integer value for employee Id!");
+
+                    if (employeeId <= 0)
+                    {
+                        throw new ArgumentException($"Please provide a valid employee value, i.e. between 1 and {this.employees.Count}!");
+                    }
+
+                    ratePerMinute = decimal.TryParse(commandParameters[2], out ratePerMinute)
+                        ? ratePerMinute
+                        : throw new ArgumentException("Please provide a valid decimal value for rate per minute!");
+                    
+                    employee = this.employees.Count >= employeeId
+                        ? this.employees[employeeId - 1]
+                        : throw new ArgumentException("This employee does not exist!");
+
+                    this.ChangeRateOfEmployee(employee, ratePerMinute);
                     break;
+
                 case "issueInvoices":
                     this.IssueInvoices();
                     break;
+
+                case "showAllEmployeesAtDepartment":
+                    if (commandParameters.Length != 2)
+                    {
+                        throw new NotSupportedException(
+                            "Show Employees At Department command must be with only 2 parameters!");
+                    }
+
+                    if (!Enum.TryParse(commandParameters[1], out department))
+                    {
+                        throw new ArgumentException("Department not valid!");
+                    }
+
+                    this.ListEmployeesAtDepartment(department);
+                    break;
+
+                case "changeEmployeePosition":
+                    if (commandParameters.Length != 3)
+                    {
+                        throw new NotSupportedException(
+                            "Change employer position command must be with only 3 parameters!");
+                    }
+
+                    if (this.employees.Count == 0)
+                    {
+                        throw new InvalidOperationException("No employees currently in the service!");
+                    }
+
+                    employeeId = int.TryParse(commandParameters[1], out employeeId)
+                        ? employeeId
+                        : throw new ArgumentException("Please provide a valid integer value for employee Id!");
+
+                    if (employeeId <= 0)
+                    {
+                        throw new ArgumentException($"Please provide a valid employee value, i.e. between 1 and {this.employees.Count}!");
+                    }
+
+                    employee = this.employees.Count >= employeeId
+                        ? this.employees[employeeId - 1]
+                        : throw new ArgumentException("This employee does not exist!");
+
+                    position = commandParameters[2];
+                    this.ChangePositionOfEmployee(employee, position);
+                    break;
+
+                case "addEmpoloyeeResponsibility":
+
+                    if (commandParameters.Length < 3)
+                    {
+                        throw new NotSupportedException(
+                            "Add responsibility command must be at least 3 parameters!");
+                    }
+
+                    if (this.employees.Count == 0)
+                    {
+                        throw new InvalidOperationException("No employees currently in the service!");
+                    }
+
+                    employeeId = int.TryParse(commandParameters[1], out employeeId)
+                        ? employeeId
+                        : throw new ArgumentException("Please provide a valid integer value for employee Id!");
+
+                    if (employeeId <= 0)
+                    {
+                        throw new ArgumentException($"Please provide a valid employee value, i.e. between 1 and {this.employees.Count}!");
+                    }
+
+                    employee = this.employees.Count >= employeeId
+                        ? this.employees[employeeId - 1]
+                        : throw new ArgumentException("This employee does not exist!");
+
+                   var responsibilitiesToAdd = commandParameters.Skip(2).ToArray();
+                    this.AddResponsibilitiesToEmployee(employee, responsibilitiesToAdd);
+
+                    break;
+
+                case "removeEmpoloyeeResponsibility":
+
+                    if (commandParameters.Length < 3)
+                    {
+                        throw new NotSupportedException(
+                            "Remove responsibility command must be at least 3 parameters!");
+                    }
+
+                    if (this.employees.Count == 0)
+                    {
+                        throw new InvalidOperationException("No employees currently in the service!");
+                    }
+
+                    employeeId = int.TryParse(commandParameters[1], out employeeId)
+                        ? employeeId
+                        : throw new ArgumentException("Please provide a valid integer value for employee Id!");
+
+                    if (employeeId <= 0)
+                    {
+                        throw new ArgumentException($"Please provide a valid employee value, i.e. between 1 and {this.employees.Count}!");
+                    }
+
+                    employee = this.employees.Count >= employeeId
+                        ? this.employees[employeeId - 1]
+                        : throw new ArgumentException("This employee does not exist!");
+
+                    var responsibilitiesToRemove = commandParameters.Skip(2).ToArray();
+                    this.RemoveResponsibilitiesToEmployee(employee, responsibilitiesToRemove);
+
+                    break;
+
                 default:
                     throw new InvalidOperationException();
             }
+        }
+
+        private void RemoveResponsibilitiesToEmployee(IEmployee employee, string[] responsibilitiesToRemove)
+        {
+            var responsibilitesToAdd = new List<ResponsibilityType>();
+            foreach (var responsibility in responsibilitiesToRemove)
+            {
+                ResponsibilityType currentResponsibility;
+                if (!Enum.TryParse(responsibility, out currentResponsibility))
+                {
+                    throw new ArgumentException($"Responsibility {responsibility} not valid!");
+                }
+                responsibilitesToAdd.Add(currentResponsibility);
+            }
+            employee.RemoveResponsibilities(responsibilitesToAdd);
+        }
+
+        private void AddResponsibilitiesToEmployee(IEmployee employee, string[] responsibilities)
+        {
+            var responsibilitesToAdd = new List<ResponsibilityType>();
+            foreach (var responsibility in responsibilities)
+            {
+                ResponsibilityType currentResponsibility;
+                if (!Enum.TryParse(responsibility, out currentResponsibility))
+                {
+                    throw new ArgumentException($"Responsibility {responsibility} not valid!");
+                }
+                responsibilitesToAdd.Add(currentResponsibility);
+            }
+            employee.AddResponsibilities(responsibilitesToAdd);
+        }
+
+        private void ChangePositionOfEmployee(IEmployee employee, string position)
+        {
+            employee.ChangePosition(position);
+
+            Console.WriteLine($"Position of employee {employee.FirstName} {employee.LastName} was successfully set to {position}");
+        }
+
+        private void ListEmployeesAtDepartment(DepartmentType department)
+        {
+            if (this.employees.Any(x => x.Department != department))
+            {
+                throw new ArgumentException($"The are no employees at department: {department}!");
+            }
+
+            StringBuilder str = new StringBuilder();
+
+            str.AppendLine($"Employees at: {department} department:");
+            foreach (var employee in employees)
+            {
+                var counter = 1;
+                if (employee.Department == department)
+                {
+
+                    str.AppendLine($"{counter}. {employee.ToString()}");
+                    counter++;
+                    //TODO ADD DISTINGUISHER BETWEEN LINES (#########?)
+                }
+            }
+            Console.WriteLine(str.ToString());
+        }
+
+        private void ChangeRateOfEmployee(IEmployee employee, decimal ratePerMinute)
+        {
+            employee.ChangeRate(ratePerMinute);
+            Console.WriteLine($"Rate per minute of employee {employee.FirstName} {employee.LastName} was successfully set to {ratePerMinute} $");
         }
 
         private void ShowEmployees()
