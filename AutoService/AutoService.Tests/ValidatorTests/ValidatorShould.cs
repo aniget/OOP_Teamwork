9540d3760859b53fd208eb6014e3498a039c5b4f
Alexander Parvanov <aparvanov@abv.bg>
2018-02-08 18:22:06 +0200

    Added NullObject unit tests.

--- AutoService/AutoService.Tests/ValidatorTests/ValidatorShould.cs
+++ AutoService/AutoService.Tests/ValidatorTests/ValidatorShould.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using AutoService.Core.Contracts;
 using AutoService.Core.Validator;
-using AutoService.Models.Assets;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -11,12 +10,22 @@ namespace AutoService.Tests.ValidatorTests
     public class ValidatorShould
     {
         [TestMethod]
-        public void ThrowException_WhenCheckNullObject_IsProvidedNullValue()
+        public void ThrowException_WhenCheckNullObjectMethod_IsProvidedNullValue()
         {
             var fakeWriter = new Mock<IWriter>();
             var mockValidator = new ValidateCore(fakeWriter.Object);
-
+            
             Assert.ThrowsException<ArgumentException>(() => mockValidator.CheckNullObject(null));
+        }
+        [TestMethod]
+        public void ThrowException_WhenCheckNullObjectMethod_IsProvidedStringOfNullValues()
+        {
+            var fakeWriter = new Mock<IWriter>();
+            var mockValidator = new ValidateCore(fakeWriter.Object);
+            var nullList = new object[] { null, null };
+
+            Assert.ThrowsException<ArgumentException>(() => mockValidator.CheckNullObject(nullList));
+            
         }
     }
 }
