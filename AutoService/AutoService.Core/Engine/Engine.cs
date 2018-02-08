7aff5d1b9fa035e8498852f4bc81ce7773a7d4b0
Alexander Parvanov <aparvanov@abv.bg>
2018-02-08 13:14:19 +0200

    Refactor and test the code.

--- AutoService/AutoService.Core/Engine/Engine.cs
+++ AutoService/AutoService.Core/Engine/Engine.cs
@@ -1,18 +1,11 @@
 ﻿using AutoService.Models.Assets;
-using AutoService.Models.Assets.Contracts;
 using AutoService.Models.BusinessProcess.Contracts;
 using AutoService.Models.Common.Contracts;
-using AutoService.Models.Common.Enums;
-using AutoService.Models.Common.Models;
 using AutoService.Models.CustomExceptions;
 using AutoService.Models.Validator;
-using AutoService.Models.Vehicles.Contracts;
-using AutoService.Models.Vehicles.Enums;
-using AutoService.Models.Vehicles.Models;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using AutoService.Core.Contracts;
 using AutoService.Core.Validator;
 
@@ -108,21 +101,6 @@ namespace AutoService.Core
         private string[] ParseCommand(string command)
         {
             return command.Split(new string[] { ";" }, StringSplitOptions.None);
-        }
-
-        private void ExecuteSingleCommand(string[] commandParameters)
-        {
-            string commandType = string.Empty;
-            try
-            {
-                this.writer.Write("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=" + Environment.NewLine);
-                commandType = commandParameters[0];
-
-            }
-            catch
-            {
-                //this catch the IndexOutOfRange Exception and throws the error message of the Default of the Switch
-            }
         }
     }
 }
