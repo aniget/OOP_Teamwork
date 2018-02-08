1d5b8d9c3dc27d97310366d3f552f27148e382e0
Alexander Parvanov <aparvanov@abv.bg>
2018-02-08 18:00:28 +0200

    Added manager classes for Employee and Invoice

---
+++ AutoService/AutoService.Core/Contracts/IEmployeeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using AutoService.Models.Common.Contracts;
+using AutoService.Models.Common.Enums;
+
+namespace AutoService.Core.Contracts
+{
+    public interface IEmployeeManager
+    {
+        void SetEmployee(IEmployee employee);
+
+        void ChangeSalary(decimal salary);
+
+        void AddResponsibilities(IList<ResponsibilityType> value);
+
+        void RemoveResponsibilities(IList<ResponsibilityType> value);
+
+        void ChangePosition(string position);
+
+        void FireEmployee();
+
+        void ChangeRate(decimal ratePerMinutes);
+    }
+}
