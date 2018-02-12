bf08fc4881906e01eade19e42031847785850066
Alexander Parvanov <aparvanov@abv.bg>
2018-02-12 20:52:57 +0200

    Refactor EmployeeShould class

--- AutoService/AutoService.Tests/ModelsTests/EmployeeShould.cs
+++ AutoService/AutoService.Tests/ModelsTests/EmployeeShould.cs
@@ -26,11 +26,11 @@ namespace AutoService.Tests.ModelsTests
         {
             //Arrange
             var validator = new Mock<IValidateModel>();
-            
+
             var employee = new Employee("firstname", "testlastname", "testposition", 1000, 10, DepartmentType.Management, validator.Object);
             //Act
             employee.FirstName = "new name";
-            
+
             //Assert
             validator.Verify(x => x.StringForNullEmpty(It.IsAny<string>()), Times.Exactly(4)); // FirstName, LastName, Position + the change of FirstName
         }
